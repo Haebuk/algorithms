29e00fb527e12e6a40e7dcfa3df0f58d004884c3
Jaesung Ryu <cwryu6252@naver.com>
2021-04-24 23:46:18 +0900

    세 수의 합 투 포인터 추가

--- Algorithms95/세 수의 합.py
+++ Algorithms95/세 수의 합.py
@@ -20,6 +20,37 @@ def sumThree(nums):
 
   return results
 
+# 풀이 2. 투 포인터로 합 계산
+def sumThree(nums):
+  results = []
+  nums.sort()
+
+  for i in range(len(nums - 2)):
+    # 중복된 값 건너뛰기
+    if i > 0 and nums[i] == nums[i - 1]:
+      continue
+      
+    # 간격을 좁혀가며 sum 계산
+    left, right = i + 1, len(nums) - 1
+    while left < right:
+      sum = nums[i] + nums[left] + nums[right]
+      if sum < 0:
+        left += 1
+      elif sum > 0:
+        right -= 1
+      else:
+        # sum = 0인 경우이므로 정답
+        results.append((nums[i], nums[left], nums[right]))
+        # 양 옆 동일한 값 스킵
+        while left < right and nums[left] == nums[left + 1]:
+          left += 1
+        while left < right and nums[right] == nums[right - 1]:
+          right -= 1
+        # sum = 0인 상황이므로 한 쪽만 이동하면 어차피 sum=0 될 수 없으므로 둘 다 이동
+        left += 1
+        right -= 1
+
 
 nums = [-1, 0, 1, 2, -1, 4]
 print(sumThree(nums))
+
