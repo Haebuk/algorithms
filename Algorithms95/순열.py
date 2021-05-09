ea1e305916543af41e443c26f0b503d88f4b2244
Jaesung Ryu <cwryu6252@naver.com>
2021-05-09 23:14:13 +0900

    Create 순열.py

---
+++ Algorithms95/순열.py
@@ -0,0 +1,29 @@
+# 서로 다른 정수를 입력받아 가능한 모든 순열을 리턴하라
+
+# 풀이 1. DFS를 활용한 순열 생성
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
+        
+        def dfs(elements):
+            # 리프 노드일 때 결과 추가
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+                
+            # 순열 생성 재귀 호출
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+                
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+                
+        dfs(nums)
+        return results
+    
+    # 풀이 2. itertools 이용한 풀이
+    class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
