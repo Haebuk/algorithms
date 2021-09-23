dcf05c24b6742e8208ccae04881122673e80c853
Haebuk <cwryu6252@naver.com>
2021-09-23 09:19:06 +0900

    미친 풀이 추가

--- leetcode/Spiral Matrix.py
+++ leetcode/Spiral Matrix.py
@@ -11,3 +11,30 @@ class Solution:
             matrix[y][x] = -1000
             x, y = x + dx, y + dy
         return answer
+
+# 미친 풀이
+def spiralOrder(self, matrix):
+    return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+"""
+spiral_order([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+= [1, 2, 3] + spiral_order([[6, 9],
+                            [5, 8],
+                            [4, 7]])
+
+= [1, 2, 3] + [6, 9] + spiral_order([[8, 7],
+                                     [5, 4]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + spiral_order([[4],
+                                              [5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + spiral_order([[5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + spiral_order([])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + []
+
+= [1, 2, 3, 6, 9, 8, 7, 4, 5]
+"""
